6d0a6e7949558ab8501c0c4bfef80a81270b83f7
Chase Yakaboski <yakaboskic@gmail.com>
2024-06-21 18:58:39 -0400

    Added training script.

--- scripts/train_detection.py
+++ scripts/train_detection.py
@@ -26,26 +26,43 @@ def make_training_datasets(args):
     eigmodel = load_model(args.path_to_eig_model)
     Xa, ya = [], []
     for target in a.keys():
-        for run in a[target].keys():
-            for node in a[target][run].keys():
-                for frame in a[target][run][node]['power']:
+        for item in a[target]:
+            for spectras in item['power']:
+                for frame in spectras:
                     Xa.append(project(frame, eigmodel['acoustic_eigvectors'], eigmodel['acoustic_spectras_avg']))
                     ya.append(target)
     Xs, ys = [], []
     for target in s.keys():
-        for run in s[target].keys():
-            for node in s[target][run].keys():
-                for frame in s[target][run][node]['power']:
+        for item in s[target]:
+            for spectras in item['power']:
+                for frame in spectras:
                     Xs.append(project(frame, eigmodel['seismic_eigvectors'], eigmodel['seismic_spectras_avg']))
                     ys.append(target)
     Xa, ya = np.array(Xa), np.array(ya)
     Xs, ys = np.array(Xs), np.array(ys)
     return Xa, ya, Xs, ys
 
+def train_classifers(args):
+    Xa, ya, Xs, ys = make_training_datasets(args)
+    class_weights_a = compute_class_weight('balanced', classes=np.unique(ya), y=ya)
+    class_weights_s = compute_class_weight('balanced', classes=np.unique(ys), y=ys)
+    amodel = MODEL.copy()
+    smodel = MODEL.copy()
+    amodel.compile(optimizer=Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    amodel.fit(Xa, ya, class_weight=class_weights_a, epochs=10)
+    amodel.save(os.path.join(args.output_dir, 'aclassifier.h5'))
+    smodel.compile(optimizer=Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    smodel.fit(Xs, ys, class_weight=class_weights_s, epochs=10)
+    smodel.save(os.path.join(args.output_dir, 'sclassifier.h5'))
+    return
+
+def run(args):
+    train_classifers(args)
+    return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--data_dir', type=str, default='data')
+    parser.add_argument('-d', '--data_dir', type=str, default='data/targets/close')
     parser.add_argument('-o', '--output_dir', type=str, default='output')
     parser.add_argument('-e', '--path_to_eig_model', type=str, default='models/target_eig_model')
     args = parser.parse_args()
